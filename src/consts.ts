1d2f8534a97a3de7443d169cb47d280df93344aa
douglastofoli <tofoli.douglas@hotmail.com>
2026-06-10 15:20:58 -0300

    first commit

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,15 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Douglas Tofoli';
+export const SITE_DESCRIPTION = 'A quiet blog about software engineering, systems, and personal projects.';
+export const AUTHOR_NAME = 'Douglas';
+export const AUTHOR_TITLE = 'Backend Engineer / Technical Writer';
+export const AUTHOR_DESCRIPTION = 'Software engineer writing about Elixir, Phoenix, distributed systems, NixOS, and personal projects.';
+
+export const SOCIAL_LINKS = {
+	github: 'https://github.com/douglastofoli',
+	linkedin: 'https://linkedin.com/in/douglastofoli',
+	x: 'https://x.com/douglex',
+	email: 'mailto:contact@douglastofoli.dev',
+};
